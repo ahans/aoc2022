e031a884ee523eaaac769296e77b9a6fe367f70b
Alexander Hans <ahans@users.noreply.github.com>
2022-12-04 15:57:44 +0100

    Cleanup

--- cpp/day04.cc
+++ cpp/day04.cc
@@ -1,72 +1,58 @@
 #include <vectorclass.h>
 
-#include <algorithm>
 #include <array>
 #include <chrono>
 #include <fstream>
 #include <iostream>
-#include <numeric>
 #include <string>
-#include <vector>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     int num_trials{1};
     if (argc > 1) num_trials = std::atoi(argv[1]);
-    auto begin{std::chrono::high_resolution_clock::now()};
 
-    for (auto t{0}; t < num_trials; ++t)
-    {
+    auto const begin{std::chrono::high_resolution_clock::now()};
+
+    for (auto t{0}; t < num_trials; ++t) {
         std::ifstream fin{"../inputs/04.txt"};
         int p1{}, p2{};
-        std::array<char, 32> a0, a1, b0, b1;
-        auto process_batch = [&] {
-            Vec32c aa0, aa1, bb0, bb1;
-            aa0.load(a0.data());
-            aa1.load(a1.data());
-            bb0.load(b0.data());
-            bb1.load(b1.data());
-            p1 += horizontal_count(aa0 <= bb0 && bb1 <= aa1 || bb0 <= aa0 && aa1 <= bb1);
-            p2 += horizontal_count(!(aa1 < bb0 || bb1 < aa0));
+	std::array<std::array<char, 64>, 4> a;
+        auto const process_batch = [&a, &p1, &p2] {
+	    std::array<Vec32c, 4> aa;
+	    for (auto k{0}; k < 4; ++k) aa[k].load(a[k].data());
+            p1 += horizontal_count(aa[0] <= aa[2] && aa[3] <= aa[1] || aa[2] <= aa[0] && aa[1] <= aa[3]);
+            p2 += horizontal_count(!(aa[1] < aa[2] || aa[3] < aa[0]));
         };
         int i{};
-        for (std::string line; std::getline(fin, line);)
-        {
+        for (std::string line; std::getline(fin, line);) {
             int p{};
-            a0[i] = line[p++] - '0';
-            if (line[p] != '-')
-                a0[i] = a0[i] * 10 + (line[p++] - '0');
-            ++p; // skip -
-            a1[i] = line[p++] - '0';
-            if (line[p] != ',')
-                a1[i] = a1[i] * 10 + (line[p++] - '0');
-            ++p; // skip ,
+            a[0][i] = line[p++] - '0';
+            if (line[p] != '-') a[0][i] = a[0][i] * 10 + (line[p++] - '0');
+            ++p;
+            a[1][i] = line[p++] - '0';
+            if (line[p] != ',') a[1][i] = a[1][i] * 10 + (line[p++] - '0');
+            ++p;
 
-            b0[i] = line[p++] - '0';
-            if (line[p] != '-')
-                b0[i] = b0[i] * 10 + (line[p++] - '0');
-            ++p; // skip -
-            b1[i] = line[p++] - '0';
-            if (p < line.length())
-                b1[i] = b1[i] * 10 + (line[p] - '0');
+            a[2][i] = line[p++] - '0';
+            if (line[p] != '-') a[2][i] = a[2][i] * 10 + (line[p++] - '0');
+            ++p;
+            a[3][i] = line[p++] - '0';
+            if (p < line.length()) a[3][i] = a[3][i] * 10 + (line[p] - '0');
 
             ++i;
 
-            if (i == 32)
-            {
+            if (i == 32) {
                 i = 0;
                 process_batch();
             }
         }
-        for (; i < 32; ++i)
-        {
-            a0[i] = a1[i] = 0;
-            b0[i] = b1[i] = 1;
+        for (; i < 32; ++i) {
+            a[0][i] = a[1][i] = 0;
+            a[1][i] = a[2][i] = 1;
         }
         process_batch();
 
         std::cout << p1 << " " << p2 << "\n";
     }
-    auto end{std::chrono::high_resolution_clock::now()};
+    auto const end{std::chrono::high_resolution_clock::now()};
     std::cerr << "took " << (end - begin).count() << std::endl;
 }
