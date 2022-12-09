60b6211271e7400c06981ae19e3b6e5b4df154f6
Alexander Hans <ahans@users.noreply.github.com>
2022-12-10 00:05:02 +0100

    Fix -Wparenthesis

--- cpp/days/day04.cc
+++ cpp/days/day04.cc
@@ -9,18 +9,18 @@
 #include <string>
 
 Result day04() {
-    auto const input = read_file("../inputs/04.txt");
+    auto const input{read_file("../inputs/04.txt")};
     int p1{}, p2{};
     constexpr bool vectorize{true};
     constexpr auto batch_size{64};
     std::array<std::array<char, batch_size>, 4> a;
-    auto const process_batch = [&a, &p1, &p2] {
+    auto const process_batch{[&a, &p1, &p2] {
         std::array<Vec64c, 4> aa;
         for (auto k{0}; k < 4; ++k)
             aa[k].load(a[k].data());
-        p1 += horizontal_count(aa[0] <= aa[2] && aa[3] <= aa[1] || aa[2] <= aa[0] && aa[1] <= aa[3]);
+        p1 += horizontal_count((aa[0] <= aa[2] && aa[3] <= aa[1]) || (aa[2] <= aa[0] && aa[1] <= aa[3]));
         p2 += horizontal_count(!(aa[1] < aa[2] || aa[3] < aa[0]));
-    };
+    }};
     int i{};
     for (auto p = input.cbegin(); p < input.cend();) {
         p += parse<char, 1, 2>(&*p, a[0][i]);
@@ -40,7 +40,7 @@ Result day04() {
                 process_batch();
             }
         } else {
-            if (a[0][i] <= a[2][i] && a[3][i] <= a[1][i] || a[2][i] <= a[0][i] && a[1][i] <= a[3][i])
+            if ((a[0][i] <= a[2][i] && a[3][i] <= a[1][i]) || (a[2][i] <= a[0][i] && a[1][i] <= a[3][i]))
                 p1 += 1;
             if (!(a[1][i] < a[2][i] || a[3][i] < a[0][i]))
                 p2 += 1;
