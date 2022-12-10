9dc4d698a6b307f8833dc4eae4fc9741f8feaab6
Alexander Hans <ahans@@users.noreply.github.com>
2022-12-10 11:42:48 +0100

    Add Python solution for day 10

---
+++ python/day10.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.11
+
+class VM:
+    def __init__(self, instructions):
+        self.x = self._x = 1
+        self.cycle = self.cycles_left = 0
+        self.instructions = instructions
+
+    def step(self):
+        self.cycle += 1
+        if self.cycles_left:
+            self.cycles_left -= 1
+        else:
+            self.x = self._x
+            try:
+                match next(self.instructions).split():
+                    case "noop", _: pass
+                    case "addx", num:
+                        self._x += int(num)
+                        self.cycles_left = 1
+            except StopIteration:
+                return False
+        return True
+
+
+vm = VM(l.strip() for l in open("../inputs/10.txt").readlines())
+p1 = 0
+p2 = [['.'] * 40 for _ in range(6)]
+while vm.step():
+    p1 += vm.cycle * vm.x if vm.cycle in (20, 60, 100, 140, 180, 220)  else 0
+    y, x = (vm.cycle - 1) // 40, (vm.cycle - 1) % 40
+    p2[y][x] = '#' if x in range(vm.x - 1, vm.x + 2) else ' '
+
+print(p1)
+print('\n'.join(''.join(r) for r in p2))
