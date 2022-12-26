f6a85978b5a9f6ed2f113f7153aaaa028a7fc62c
Alexander Hans <ahans@@users.noreply.github.com>
2022-12-26 12:18:54 +0100

    Beautify day 25 Python solution

--- python/day25.py
+++ python/day25.py
@@ -6,17 +6,7 @@ def from_snafu(n):
     return sum(5**i * CM[c] for i, c in enumerate(reversed(n)))
 
 def to_snafu(d):
-    if d == 0:
-        return "0"
-    s = ""
-    while d > 0:
-        r = d % 5
-        match r:
-            case 4: s = "-" + s; r = -1
-            case 3: s = "=" + s; r = -2
-            case _: s = str(r) + s
-        d = (d - r) // 5
-    return s
+    return "" if d == 0 else to_snafu((d + 2) // 5) + "012=-"[d % 5]
 
 
 print(to_snafu(sum(from_snafu(l.strip()) for l in open("../inputs/25.txt").readlines())))
